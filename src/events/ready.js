e252b6e4de8e3f6b43fbe9f9fbcbf006f9aa5b82
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-11 05:43:40 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,5 +6,10 @@ module.exports = {
 
     await client.mongo.connect();
     await client.mongo.db("client").collection("conn").updateOne({ _id: client.user.id }, { $set: { timestamp: client.readyTimestamp } }, { upsert: true });
+
+    setInterval(async () => {
+      const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: client.user.id });
+      if (timestamp !== client.readyTimestamp) return await process.exit(0);
+    }, 1000)
   }
 };
