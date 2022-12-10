bd86bfbc12230edd1214507743be6353c7a90837
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-11 05:36:28 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
   once: true,
   async execute(client) {
     await console.log(`Connected! ☔`);
-    await client.mongo.db("client").collection("conn").updateOne({ _id: client.user.id }, { $set: { timestamp: client.readyTimestamp } }, { upsert: true });
+    await client.mongo.database("client").collection("conn").updateOne({ _id: client.user.id }, { $set: { timestamp: client.readyTimestamp } }, { upsert: true });
   }
 };
