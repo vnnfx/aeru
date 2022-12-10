64706003854d03a76f3359ea760e34010cb94f7d
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-11 05:32:32 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,7 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds
+  ]
 });
 
 client.commands = new Collection();
