13f9c9ba21ae77c437859d30d12a753ad9287988
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-11 05:30:27 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,4 @@
+const { readdirSync } = require("fs");
 const { MongoClient } = require("mongodb");
 const { Client, Collection } = require("discord.js");
 
@@ -8,6 +9,21 @@ const client = new Client({
 
 client.commands = new Collection();
 client.mongo = new MongoClient({ url: process.env["MONGO_URI"] });
+
+const events = readdirSync("./src/events").filter(f => f.endsWith(".js"));
+for (const file of events) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+};
+
+setInterval(async () => {
+  const { timestamp } = await client.mongo.db("client").collection("conn").findOne({ _id: client.user.id });
+  if (timestamp !== bot.readyTimestamp) return await process.exit(0);
+}, 1000)
 
 client.login(process.env["DISCORD_TOKEN"]);
 setTimeout(() => process.exit(0), 5 * 60 * 60 * 1000)
