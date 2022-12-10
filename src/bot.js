4436cb58b0c259319b29ad31015d385ec5c2ecb3
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-11 05:21:55 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,13 @@
-const { Client } = require("discord.js");
-const client = new Client({ intents: [] });
+const { MongoClient } = require("mongodb");
+const { Client, Collection } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+});
+
+client.commands = new Collection();
+client.mongo = new MongoClient({ url: process.env["MONGO_URI"] });
 
 client.login(process.env["DISCORD_TOKEN"]);
-setTimeout(() => process.exit(0), 30 * 1000)
+setTimeout(() => process.exit(0), 5 * 60 * 60 * 1000)
