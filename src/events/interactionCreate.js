c2714228222b7887d4acd44b626f809c81c369ca
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-11 10:36:25 +0700

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,7 +6,7 @@ module.exports = {
     const { timestamp } = await interaction.client.mongo.db("client").collection("conn").findOne({ _id: interaction.client.user.id });
     if (timestamp !== interaction.client.readyTimestamp) return await process.exit(0);
 
-    if (!interaction.isCommand()) {
+    if (interaction.isCommand()) {
       const command = await interaction.client.commands.find(command => command.data.name === interaction.commandName && command.data.type === [undefined, ApplicationCommandType.User, ApplicationCommandType.Message][interaction.commandType - 1]);
       if (command) return await command.execute(interaction);
     }
