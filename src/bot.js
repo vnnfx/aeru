fd1d7ab9ff550bad3b1910bacf0cb0a132650925
ゾクル <111561391+zokuru@users.noreply.github.com>
2022-12-12 23:06:58 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -6,7 +6,7 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntents.MessageContent
+    GatewayIntentBits.MessageContent
   ]
 });
 
